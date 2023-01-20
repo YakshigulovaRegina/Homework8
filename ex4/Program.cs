b4de9cdcaf0202f1160738e88780f9f6842d2a67
Regina <gumerovars@gmail.com>
2023-01-20 15:59:00 +0500

    fourth task

---
+++ ex4/Program.cs
@@ -0,0 +1,39 @@
+﻿int[,,] generateArray(int m, int n, int l)
+{
+    int[,,] arr = new int[m, n, l];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < l; k++)
+            {
+                arr[i, j, k] = new Random().Next(10, 99);
+            }
+        }
+    }
+    return arr;
+}
+
+void printArray(int[,,] arr)
+{
+    for (int k = 0; k < arr.GetLength(2); k++)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{(arr[i, j, k])} " + (i, j, k) + " ; ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.WriteLine("Введите число строк матрицы ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов матрицы ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите глубину матрицы ");
+int l = Convert.ToInt32(Console.ReadLine());
+int[,,] arr = generateArray(m, n, l);
+printArray(arr);
